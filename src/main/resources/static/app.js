07579ec173328bc40adc90bd5d3da95b3037550b
suci <telefonosuci@gmail.com>
2018-03-06 14:15:50 +0100

    Ciao Pippo

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,5 +4,7 @@ var helloApp = angular.module("helloApp", []);
 helloApp.controller("HelloCtrl", function($scope) {
 
     $scope.name = "Calvin Hobbes";
-
+    $scope.model = {
+        nome:"Pippo"
+    }
 });
