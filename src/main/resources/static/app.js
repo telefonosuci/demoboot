7a8979a0605b9acfcbc57c5602fe4d26a654bd02
suci <telefonosuci@gmail.com>
2018-03-06 18:25:27 +0100

    Added template folder

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -6,5 +6,9 @@ helloApp.controller("HelloCtrl", function($scope) {
     $scope.name = "Calvin Hobbes";
     $scope.model = {
         nome:"Pippo"
-    }
+    };
+
+    $scope.template="templates/main.html";
+
+
 });
