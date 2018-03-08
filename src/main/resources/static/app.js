6751e663e0494a5c38d9d9aeee590429aa8abf05
suci <telefonosuci@gmail.com>
2018-03-08 18:17:12 +0100

    Added second controller

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,11 +4,40 @@ var helloApp = angular.module("helloApp", []);
 helloApp.controller("HelloCtrl", function($scope) {
 
     $scope.name = "Calvin Hobbes";
+
     $scope.model = {
         nome:"Pippo"
     };
 
     $scope.template="templates/main.html";
 
+    $scope.model.eventForm={
+        name:"",
+        date:"",
+        description:""
+    };
+
+});
+
+helloApp.controller("MenuCtrl", function($scope) {
+
+    $scope.pages = [
+        {
+            id:1,
+            name:"Home"
+        },
+        {
+            id:2,
+            name:"About"
+        },
+        {
+            id:1,
+            name:"Contact"
+        }
+    ];
+
+    $scope.title = "My Blog";
+
+    $scope.template="templates/main.html";
 
 });
